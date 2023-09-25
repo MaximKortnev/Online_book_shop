8c318bb7b02d4a17c9654fcfc1eef9dda4d60d28
MaximKortnev <fafccc@gmail.com>
2023-09-25 09:43:02 +0300

    Добавлены методы поиска товара по id ( SearchProductById) и получения списка всех товаров (GetProducts).

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -1,75 +1,20 @@
 ﻿using System.Collections.Generic;
-using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using OnlineShopWebApp.Models;
-using System.IO;
-using Newtonsoft.Json;
-
 
 namespace OnlineShopWebApp.Controllers
 {
-    public class Product
-    {
-        [JsonProperty("id")]
-        public int Id { get; set; }
-
-        [JsonProperty("name")]
-        public string Name { get; set; }
-
-        [JsonProperty("cost")]
-        public decimal Cost { get; set; }
-
-        [JsonProperty("description")]
-        public string Description { get; set; }
-    }
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        private List<Product> ReadProductsFromJson(string filePath)
-        {
-            string json;
-
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                json = reader.ReadToEnd();
-            }
-
-            return JsonConvert.DeserializeObject<List<Product>>(json);
-        }
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
 
         public string Index()
         {
-            string jsonFilePath = "data.json";
-            List<Product> products = ReadProductsFromJson(jsonFilePath);
+            var result_str = "";
 
-            string result_str = "";
-            foreach (var product in products)
+            foreach (var product in ProductEssence.GetProducts())
             {
                     result_str += "Product ID: " + product.Id + "\nName: " + product.Name + "\nCost " + product.Cost + "\n\n";
             }
-            
             return result_str;
         }
-
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
-
-
 }
--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -7,33 +7,9 @@ namespace OnlineShopWebApp.Controllers
 {
     public class ProductController : Controller
     {
-        private List<Product> ReadProductsFromJson(string filePath)
+        public string Index(int id)
         {
-            string json;
-
-            using (StreamReader reader = new StreamReader(filePath))
-            {
-                json = reader.ReadToEnd();
-            }
-
-            return JsonConvert.DeserializeObject<List<Product>>(json);
-        }
-        public string Index(int id = -1)
-        {
-            string jsonFilePath = "data.json";
-            List<Product> products = ReadProductsFromJson(jsonFilePath);
-
-            string result_str = "";
-            foreach (var product in products)
-                {
-                    if (product.Id == id)
-                    {
-                        result_str = "Product ID: " + product.Id + "\nName: " + product.Name + "\nCost " + product.Cost + "\nDescription: " + product.Description;
-                        break;
-                    }
-                    else { result_str = "Такого товара нет"; }
-                }
-            if (id == -1) { result_str = "Вы не выбрали товар"; }
+            var result_str = ProductEssence.SearchProductById(id); 
             return result_str;
         }
     }
