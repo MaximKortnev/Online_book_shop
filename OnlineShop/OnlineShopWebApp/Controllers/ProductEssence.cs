fba95e3d36c959af7052aa58f40ce602e85e8eca
MaximKortnev <fafccc@gmail.com>
2023-09-25 09:07:33 +0300

    Добавлен файл ProductEssence. Класс Product вынесен в отдельный файл. Функционал получения продуктов также вынесен в отдельный класс.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductEssence.cs
@@ -0,0 +1,34 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class Product
+    {
+        [JsonProperty("id")]
+        public int Id { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("cost")]
+        public decimal Cost { get; set; }
+
+        [JsonProperty("description")]
+        public string Description { get; set; }
+    }
+    public static class ProductEssence
+    {
+        public static List<Product> ReadProductsFromJson(string filePath)
+        {
+            var json = "";
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                json = reader.ReadToEnd();
+            }
+            return JsonConvert.DeserializeObject<List<Product>>(json);
+        }
+    }
+}
