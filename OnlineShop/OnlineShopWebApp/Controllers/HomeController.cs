c6d67894b5e7e333c030d3b679fadceeb1cd0e83
MaximKortnev <fafccc@gmail.com>
2023-09-28 09:31:25 +0300

    Добавлена проверка корректности пути к файлу с данными продуктов. Добавлен метод  GetProductsData, для формирования списка продуктов. Добавлен метод ListEmpry, для проверки загрузки данных.

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -4,16 +4,6 @@ namespace OnlineShopWebApp.Controllers
 {
     public class HomeController : Controller
     {
-
-        public string Index()
-        {
-            var productCard = "";
-
-            foreach (var product in ProductsRepository.GetProducts())
-            {
-                productCard += "Product ID: " + product.Id + "\nName: " + product.Name + "\nCost " + product.Cost + "\n\n";
-            }
-            return productCard;
-        }
+        public string Index()=> ProductsRepository.GetProductsData();    
     }
 }
---
+++ OnlineShop/OnlineShopWebApp/Repositoryes/ProductsRepository.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public static class ProductsRepository
+    {
+        public static List<Product> ReadProductsFromJson(string filePath)
+        {
+            var json = "";
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                json = reader.ReadToEnd();
+            }
+            return JsonConvert.DeserializeObject<List<Product>>(json);
+        }
+
+        public static string SearchProductById(int id)
+        {
+            var productCard = GetProducts().FirstOrDefault(s => s.Id == id);
+
+            if (productCard == null) return "Такого товара нет";
+            return $"Product ID: {productCard.Id}\nName: {productCard.Name}\nCost {productCard.Cost}\nDescription: {productCard.Description}";
+        }
+
+        public static List<Product> GetProducts()
+        {
+            var jsonFilePath = "wwwroot/data.json";
+
+            try
+            {
+                List<Product> products = ReadProductsFromJson(jsonFilePath);
+                return products;
+            }
+            catch
+            {
+                return new List<Product> {};
+            }
+        }
+
+        public static string GetProductsData()
+        {
+
+            var productCart = "";
+            var listProducts = GetProducts();
+
+            if (ListEmpry(listProducts))
+            {
+                foreach (var product in listProducts)
+                {
+                    productCart += "Product ID: " + product.Id + "\nName: " + product.Name + "\nCost " + product.Cost + "\n\n";
+                }
+                return productCart;
+            }
+            return "Не удалось загрузить данные.";
+        }
+
+        private static bool ListEmpry(List<Product> prod) => prod.Any();
+    }
+}
