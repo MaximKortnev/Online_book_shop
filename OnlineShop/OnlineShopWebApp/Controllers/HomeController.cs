68a88e5d5e6c00d3c853282dcd48d8f98f209b2c
MaximKortnev <fafccc@gmail.com>
2023-09-28 09:01:38 +0300

    Добавлен метод вывода информации о продуктах GetProductsData. Добавлен обработчик существования/верности пути к файлу с данными продуктов.

--- OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/HomeController.cs
@@ -4,15 +4,6 @@ namespace OnlineShopWebApp.Controllers
 {
     public class HomeController : Controller
     {
-        public string Index()
-        {
-            var productCart = "";
-
-            foreach (var product in ProductRepository.GetProducts())
-            {
-                productCart += "Product ID: " + product.Id + "\nName: " + product.Name + "\nCost " + product.Cost + "\n\n";
-            }
-            return productCart;
-        }
+        public string Index() => ProductRepository.GetProductsData();
     }
 }
---
+++ OnlineShop/OnlineShopWebApp/Repositoryes/ProductRepository.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq.Expressions;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public static class ProductRepository
+    {
+        public static List<Product> ReadProductsFromJson(string filePath)
+        {
+            var json = "";
+
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                json = reader.ReadToEnd();
+            }
+            return JsonConvert.DeserializeObject<List<Product>>(json);
+        }
+
+        public static List<Product> GetProducts()
+        {
+            var jsonFilePath = "wwwroot/data.json";
+
+            try
+            {
+                List<Product> products = ReadProductsFromJson(jsonFilePath);
+                return products;
+            }
+            catch 
+            {
+                return null;
+            }
+        }
+
+        public static string GetProductsData() {
+
+            var productCart = "";
+            var listProducts = ProductRepository.GetProducts();
+
+            if (listProducts != null) { 
+                foreach (var product in listProducts)
+                {
+                    productCart += "Product ID: " + product.Id + "\nName: " + product.Name + "\nCost " + product.Cost + "\n\n";
+                }
+                return productCart;
+            }
+            return "Ну удалось загрузить данные.";
+        }
+    }
+}
