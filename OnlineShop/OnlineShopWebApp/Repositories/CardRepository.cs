ceef531de3d871fa0a5cbf1f15e0a405090a4f5c
MaximKortnev <fafccc@gmail.com>
2023-10-06 22:21:28 +0300

    JS

--- OnlineShop/OnlineShopWebApp/Repositories/CardRepository.cs
+++ OnlineShop/OnlineShopWebApp/Repositories/CardRepository.cs
@@ -16,14 +16,15 @@ namespace OnlineShopWebApp.Repositories
             {
                 var newCart = new Cart()
                 {
-                    Id =Guid.NewGuid(),
+                    Id = Guid.NewGuid(),
                     UserId = userId,
                     Items = new List<CartItem>() { new CartItem() { Id = Guid.NewGuid(), Amount = 1, Product = product } }
                 };
                 carts.Add(newCart);
             }
-            else { 
-                var existingCardItem = existingCart.Items.FirstOrDefault(prod=>prod.Product.Id == product.Id);
+            else
+            {
+                var existingCardItem = existingCart.Items.FirstOrDefault(prod => prod.Product.Id == product.Id);
 
                 if (existingCardItem != null) existingCardItem.Amount += 1;
                 else existingCart.Items.Add(new CartItem() { Id = Guid.NewGuid(), Amount = 1, Product = product });
@@ -31,9 +32,4 @@ namespace OnlineShopWebApp.Repositories
         }
         public static Cart TryGetByUserId(string userId) => carts.FirstOrDefault(x => x.UserId == userId);
     }
-      
 };
-            
-            
-            
-       
