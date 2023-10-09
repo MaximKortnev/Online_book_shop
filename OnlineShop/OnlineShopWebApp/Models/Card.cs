cddc4be08c797474a4c58aae4765b0288689c622
MaximKortnev <fafccc@gmail.com>
2023-10-09 08:22:29 +0300

    Поправлено оформление

--- OnlineShop/OnlineShopWebApp/Models/Card.cs
+++ OnlineShop/OnlineShopWebApp/Models/Card.cs
@@ -6,9 +6,9 @@ namespace OnlineShopWebApp.Models
 {
     public class Cart
     {
-        public Guid Id { get; set;}
-        public string UserId { get; set;}
-        public List<CartItem> Items {get; set;}
-        public decimal Cost { get=> Items.Sum(prod=>prod.Cost);}
+        public Guid Id { get; set; }
+        public string UserId { get; set; }
+        public List<CartItem> Items { get; set; }
+        public decimal Cost { get => Items.Sum(prod => prod.Cost); }
     }
 }
