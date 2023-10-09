3781b9668b062941ba31599de969b4380e9d4447
max <fafccc@gmail.com>
2023-10-09 17:43:59 +0300

    111

--- OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
+++ OnlineShop/OnlineShopWebApp/Controllers/ProductController.cs
@@ -12,11 +12,11 @@ namespace OnlineShopWebApp.Controllers
             this.productsRepository = productsRepository;
         }
 
-        public IActionResult Index(int productId222)
+        public IActionResult Index(int productId)
         {
-            var productCard = productsRepository.TryGetProductById(productId222);
+            var productCard = productsRepository.TryGetProductById(productId);
 
-            if (productCard == null || productCard.Id != productId222) { return View("ErrorProduct");}
+            if (productCard == null || productCard.Id != productId) { return View("ErrorProduct");}
             return View(productCard);
         }
     }
