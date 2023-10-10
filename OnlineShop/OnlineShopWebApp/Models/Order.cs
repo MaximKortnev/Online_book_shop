68c8465d3d5b4f2903db7a881ddb022570acc7f8
max <fafccc@gmail.com>
2023-10-10 21:38:12 +0300

    Добавлена форма дл яоформления покупок, и соответствующий контроллер.

---
+++ OnlineShop/OnlineShopWebApp/Models/Order.cs
@@ -0,0 +1,14 @@
+﻿namespace OnlineShopWebApp.Models
+{
+    public class Order
+    {
+        public string FullName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public string DeliveryMethod { get; set; }
+        public string PaymentMethod { get; set; }
+        public string PromoCode { get; set; }
+        public string TotalCost { get; set; }
+        public Product[] Products { get; set; }
+    }
+}
--- OnlineShop/OnlineShopWebApp/wwwroot/js/site.js
+++ OnlineShop/OnlineShopWebApp/wwwroot/js/site.js
@@ -15,3 +15,45 @@ function addToCart(productId) {
         }
     });
 }
+
+function submitOrder() {
+    const fullName = document.getElementById('fullName').value;
+    const phone = document.getElementById('phone').value;
+    const email = document.getElementById('email').value;
+    const deliveryMethod = document.getElementById('deliveryMethod').value;
+    const paymentMethod = document.getElementById('paymentMethod').value;
+    const promoCode = document.getElementById('promoCode').value;
+    const totalCost = document.getElementById('totalCost').value;
+
+    const orderData = {
+        fullName: fullName,
+        phone: phone,
+        email: email,
+        deliveryMethod: deliveryMethod,
+        paymentMethod: paymentMethod,
+        promoCode: promoCode,
+        totalCost: totalCost,
+        // Добавьте информацию о товарах в заказ
+        products: [
+            { name: 'Товар 1', price: 500 },
+            { name: 'Товар 2', price: 700 }
+        ]
+    };
+
+    // Отправка данных на сервер
+    fetch('/api/order', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(orderData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Ответ от сервера:', data);
+            // Добавьте здесь логику для обработки ответа от сервера
+        })
+        .catch(error => {
+            console.error('Произошла ошибка:', error);
+        });
+}
