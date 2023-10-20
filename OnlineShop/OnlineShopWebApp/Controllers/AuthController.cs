00bdba01cf07389e12d8c323963341155f28826c
max <fafccc@gmail.com>
2023-10-20 13:48:30 +0300

    Добавлено "представление" входа и регистрации на сайте.

---
+++ OnlineShop/OnlineShopWebApp/Controllers/AuthController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OnlineShopWebApp.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(string login, string password, bool rememberMe) 
+        {
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpPost]
+        public IActionResult Registration(string login, string email, string password, string confirmPassword, string username)
+        {
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
--- OnlineShop/OnlineShopWebApp/wwwroot/js/site.js
+++ OnlineShop/OnlineShopWebApp/wwwroot/js/site.js
@@ -57,3 +57,11 @@ function submitOrder() {
             console.error('Произошла ошибка:', error);
         });
 }
+
+function openTab(tabName) {
+    var tabs = document.getElementsByClassName("tab");
+    for (var i = 0; i < tabs.length; i++) {
+        tabs[i].classList.remove("active-tab");
+    }
+    document.getElementById(tabName).classList.add("active-tab");
+}
