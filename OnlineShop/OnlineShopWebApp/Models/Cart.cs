59923a5367fe13ce51cff48ab60fd9a3eee0544e
max <fafccc@gmail.com>
2023-10-20 12:38:38 +0300

    Добавлены ViewComponents, для частичного представления Header. Добавлена возможность отображения количества товаров в корзине.

--- OnlineShop/OnlineShopWebApp/Models/Cart.cs
+++ OnlineShop/OnlineShopWebApp/Models/Cart.cs
@@ -9,6 +9,7 @@ namespace OnlineShopWebApp.Models
         public Guid Id { get; set; }
         public string UserId { get; set; }
         public List<CartItem> Items { get; set; }
-        public decimal Cost { get => Items.Sum(prod => prod.Cost); }
+        public decimal Cost { get => Items?.Sum(prod => prod.Cost) ?? 0; }
+        public decimal Amount { get => Items?.Sum(prod => prod.Amount) ?? 0; }
     }
 }
---
+++ OnlineShop/OnlineShopWebApp/Views/Shared/Components/Cart/Cart.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OnlineShopWebApp.Interfaces;
+
+namespace OnlineShopWebApp.Views.Shared.Components.Cart
+{
+    public class Cart : ViewComponent
+    {
+        private readonly ICartsRepository cartsRepository;
+
+        public Cart(ICartsRepository cartsRepository) 
+        { 
+            this.cartsRepository = cartsRepository;
+        }
+
+        public IViewComponentResult Invoke() 
+        {
+            var carts = cartsRepository.TryGetByUserId(Constants.UserId);
+            var productCounts = carts?.Amount ?? 0;
+            
+            return View("Cart", productCounts); 
+        }
+    }
+}
