eeda06f1beae6f0d97c77a41158350df1b4ad0b1
max <fafccc@gmail.com>
2023-10-28 16:20:20 +0300

    Ура, валидация заработала.

--- OnlineShop/OnlineShopWebApp/wwwroot/js/site.js
+++ OnlineShop/OnlineShopWebApp/wwwroot/js/site.js
@@ -18,7 +18,7 @@ function addToCart(productId) {
 
 function submitOrder() {
     const fullName = $('#fullName').val();
-    const address = $('#address').val()
+    const address = $('#address').val();
     const phone = $('#phone').val();
     const email = $('#email').val();
     const deliveryMethod = $('#deliveryMethod').val();
@@ -81,4 +81,30 @@ document.addEventListener("DOMContentLoaded", function () {
 
 $(document).ready(function () {
     $("#phone").inputmask();
+});
+
+document.querySelector('form').addEventListener('submit', function (e) {
+    e.preventDefault(); // Предотвратить отправку формы по умолчанию
+    const form = e.target;
+
+    fetch(form.action, {
+        method: form.method,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: new FormData(form)
+    })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log('Ответ от сервера:', data);
+            window.location.href = "/Order/OrderSuccessfully";
+        })
+        .catch(error => {
+            console.error('Произошла ошибка:', error);
+        });
 });
